59e45b7c5a044e54949ebb0c1094c3aef9db67d0
Deployment User <user@example.com>
2025-06-03 22:46:31 +0900

    Remove WordPress Backend Status test banner from homepage

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,20 +5,12 @@ import KeyHighlights from '@/components/KeyHighlights';
 import UniqueValue from '@/components/UniqueValue';
 import Newsletter from '@/components/Newsletter';
 import Footer from '@/components/Footer';
-import WordPressBackendStatus from '@/components/WordPressBackendStatus';
 
 const Index = () => {
   return (
     <div className="min-h-screen bg-white">
       <Navigation />
       <Hero />
-      
-      {/* WordPress Backend Status - Shows headless WordPress connection */}
-      <div className="py-8 bg-gray-50">
-        <div className="container mx-auto px-4">
-          <WordPressBackendStatus />
-        </div>
-      </div>
       
       <IntroBrief />
       <KeyHighlights />
